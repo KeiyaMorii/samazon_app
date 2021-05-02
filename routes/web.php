67c530ad54c740e56e171fc5a100923c8d1b94cd
KeiyaMorii <kei8.m.0123@gmail.com>
2021-05-02 22:42:38 +0900

    Top page settings

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'WebController@index');
 
 # カートの中身を確認するページへのURL
 Route::get('users/carts', 'CartController@index')->name('carts.index');
