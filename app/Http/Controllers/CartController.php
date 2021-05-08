8aab1f3b67390328b287167ea91a9dc832ee49cd
KeiyaMorii <kei8.m.0123@gmail.com>
2021-05-08 16:11:43 +0900

    Automatic shipping calculation function

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -24,7 +24,11 @@ class CartController extends Controller
         $total = 0;
 
         foreach ($cart as $c) {
-            $total += $c->qty * $c->price;
+            if($c->options->carriage) {
+                $total += ($c->qty * ($c->price + env('CARRIAGE')));
+            } else {
+                $total += $c->qty * $c->price;
+            }
         }
 
         return view('carts.index', compact('cart', 'total'));
@@ -46,6 +50,9 @@ class CartController extends Controller
                 'qty' => $request->qty,
                 'price' => $request->price,
                 'weight' => $request->weight,
+                'options' => [
+                    'carriage' => $request->carriage
+                ]
             ]
         );
 
