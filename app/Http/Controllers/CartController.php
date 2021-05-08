d0b7a573e7928a7c20711394dd369585160bf2fa
KeiyaMorii <kei8.m.0123@gmail.com>
2021-05-08 14:50:16 +0900

    Sales list screen added

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -96,22 +96,44 @@ class CartController extends Controller
      */
     public function destroy(Request $request)
     {
-        $user_shoppingcarts = DB::table('shoppingcart')->where('instance', Auth::user()->id)->get();
+        $user_shoppingcarts = DB::table('shoppingcart')->get();
+       $number = DB::table('shoppingcart')->where('instance', Auth::user()->id)->count();
 
-        # 現在までのユーザーが注文したカートの数を取得している
-        $count = $user_shoppingcarts->count();
+       $count = $user_shoppingcarts->count();
 
-        # 新しくデータベースに登録するカートのデータ用にカートのIDを一つ増やしている
-        $count += 1;
-        # ユーザーのIDを使ってカート内の商品情報などをデータベースへと保存している
-        Cart::instance(Auth::user()->id)->store;
+       $count += 1;
+       $number += 1;
+       $cart = Cart::instance(Auth::user()->id)->content();
 
-        # 購入済みのフラグをtrueにして、購入処理を行っている
-        # DB::table('shoppingcart')では、データベース内のshoppingcartへのアクセスを行っている。その後where()を使ってユーザーのIDとカート数$countを使い、先程作成したカートのデータを更新している
-        DB::table('shoppingcart')->where('instance', Auth::user()->id)->where('number', null)->update(['number' => $count, 'buy_flag' => true]);
+       $price_total = 0;
+       $qty_total = 0;
 
-        Cart::instance(Auth::user()->id)->destroy();
+       foreach ($cart as $c) {
+           if ($c->options->carriage) {
+               $price_total += ($c->qty * ($c->price + 800));
+           } else {
+               $price_total += $c->qty * $c->price;
+           }
+           $qty_total += $c->qty;
+       }
 
-        return redirect()->route('carts.index');
+       Cart::instance(Auth::user()->id)->store($count);
+
+       DB::table('shoppingcart')->where('instance', Auth::user()->id)
+                                ->where('number', null)
+                                ->update(
+                                    [
+                                        'code' => substr(str_shuffle('1234567890abcdefghijklmnopqrstuvwxyz'), 0, 10),
+                                        'number' => $number, 
+                                        'price_total' => $price_total,
+                                        'qty' => $qty_total,
+                                        'buy_flag' => true, 
+                                        'updated_at' => date("Y/m/d H:i:s")
+                                    ]
+                                );
+
+       Cart::instance(Auth::user()->id)->destroy();
+
+       return redirect()->route('carts.index');
     }
 }
